65b4b2a7e33e4926c32bd163041b60d787306c61
avishka2k <avishka2k@gmail.com>
2022-04-20 21:52:10 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+    builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        backgroundColor: Colors.white,
       ),
       home: const MyHomePage(),
     );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,155 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [                
+                      const Text("Find Your\nFavorite Food",
+                      style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,),
+                      ),
+                      
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          // ignore: prefer_const_literals_to_create_immutables
+                          boxShadow: [
+                            const BoxShadow(
+                              blurRadius: 35,                              
+                              offset: Offset(0,20),
+                              color: Colors.grey,
+                            )
+                          ]
+                        ),                      
+                      child: IconButton(
+                        onPressed: () {  }, 
+                        icon: const Icon(Icons.notification_add_outlined),
+                        iconSize: 30,
+                        color:HexColor('#15BE77'),
+                      ), 
+                      ),                                   
+                    ],
+                  ),
+                ), 
+                const SizedBox(height: 60,),
+
+           //head card     
+                Container(
+                  width: 330,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      HexColor('#53E88B'),
+                      HexColor('#15BE77'),
+                    ],),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/image1.png",),
+                      fit: BoxFit.fill,
+                      ),
+                   color: HexColor('#15BE77'),
+                   borderRadius: BorderRadius.circular(20),
+                  ),
+                  
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(width: 0),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Special Deal For \nOctober",
+                            style: TextStyle(fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            ),
+                            ),
+                            const SizedBox(height: 15,),
+                            Container(
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                primary: Colors.transparent, 
+                                shadowColor: Colors.transparent
+                                ),
+                                child: Text("Buy Now",
+                                style: TextStyle(fontSize: 15,
+                                fontWeight: FontWeight.bold, 
+                                color: HexColor('#15BE77'),
+                                ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+           // nearest restaurant
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Nearest Restaurant",
+                          // ignore: unnecessary_const
+                          style: const TextStyle(fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                          ),
+                          Text("View More",
+                          style: TextStyle(color: HexColor("#FF7C32")),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Image(image: AssetImage("assets/vegi.png"),
+                            width: 200,),
+                            Text("Vegan Resto",
+                            style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),
+                            ),
+                            Text("12 Min"),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
