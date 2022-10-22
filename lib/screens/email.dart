e153a8ac04b979212dbaefaafcc4ba87779408a0
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2022-10-22 13:48:38 +0530

    Adding email file

---
+++ lib/screens/email.dart
@@ -0,0 +1,162 @@
+// ignore_for_file: camel_case_types, prefer_final_fields
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:qr_scanner/imageResult/email_image.dart';
+import 'package:qr_scanner/theme.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// ignore: must_be_immutable, use_key_in_widget_constructors
+class qr_email extends StatelessWidget {
+  TextEditingController _email = TextEditingController();
+  TextEditingController _subject = TextEditingController();
+  TextEditingController _body = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 60),
+        child: SizedBox(
+          width: 300,
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => email_image(
+                    email: _email.text,
+                    subject: _subject.text,
+                    body: _body.text,
+                  ),
+                ),
+              );
+            },
+            shape: const BeveledRectangleBorder(
+              borderRadius: BorderRadius.zero,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                FaIcon(FontAwesomeIcons.qrcode),
+                SizedBox(width: 15),
+                Text(
+                  "Create",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),
+          IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const FaIcon(FontAwesomeIcons.arrowLeft)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Create QR Code",
+                  style: myTextStyle.title,
+                ),
+                const SizedBox(height: 20),
+                Material(
+                  borderRadius: BorderRadius.circular(10),
+                  elevation: 0.3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/icons/email.png',
+                          width: 23,
+                        ),
+                        const SizedBox(width: 10),
+                        const Text("Email", style: myTextStyle.createQrName),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10),
+                    elevation: 0.3,
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _email,
+                          style: GoogleFonts.comfortaa(fontSize: 20),
+                          decoration: const InputDecoration(
+                            hintStyle: TextStyle(fontSize: 18),
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10),
+                    elevation: 0.3,
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _subject,
+                          style: GoogleFonts.comfortaa(fontSize: 20),
+                          decoration: const InputDecoration(
+                            hintStyle: TextStyle(fontSize: 18),
+                            border: InputBorder.none,
+                            hintText: 'Subject',
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10),
+                    elevation: 0.3,
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _body,
+                          maxLines: 8,
+                          style: GoogleFonts.comfortaa(fontSize: 20),
+                          decoration: const InputDecoration(
+                            hintStyle: TextStyle(fontSize: 18),
+                            border: InputBorder.none,
+                            hintText: 'Body',
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
