9e58ada16848f78737dcadd31d3c79e2eecdd784
NaveenSilva <naveensilva000@gmail.com>
2022-10-23 19:09:06 +0530

    updated

--- lib/screens/nv/login.dart
+++ lib/screens/nv/login.dart
@@ -142,62 +142,136 @@ class _loginState extends State<login> {
                                 ),
                               ),
                             ),
-                            SizedBox(height: 15.h),
-                            Container(
-                              decoration: BoxDecoration(boxShadow: [
-                                BoxShadow(
-                                  blurRadius: 100,
-                                  offset: const Offset(0, 20),
-                                  color: HexColor('#15BE77').withOpacity(0.1),
-                                )
-                              ]),
-                              child: TextFormField(
-                                validator: (value) {
-                                  if (value!.length <= 5) {
-                                    return 'Minimum 5 characters';
-                                  }
-                                  return null;
-                                },
-                                controller: _passwordControllar,
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor: Colors.white,
-                                  enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        width: 0.2.w,
-                                        color: Colors.grey.withOpacity(0.5),
-                                      ),
-                                      borderRadius: BorderRadius.circular(15)),
-                                  hintText: '',
-                                  labelText: 'Password',
-                                  labelStyle: const TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: HexColor('#15BE77'),
-                                      ),
-                                      borderRadius: BorderRadius.circular(15)),
-                                  focusedErrorBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: HexColor('#15BE77'),
-                                      ),
-                                      borderRadius: BorderRadius.circular(15)),
-                                  errorBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        width: 0.2.w,
-                                        color: Colors.red,
-                                      ),
-                                      borderRadius: BorderRadius.circular(15)),
-                                ),
-                              ),
-                            ),
+                            SizedBox(height: 15.h),                          
                           ],
                         ),
                       ),
                     ),
-                    
+                    RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          signIn();
+                        }
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                      padding: const EdgeInsets.all(0.0),
+                      child: primatyButton(btnText: 'Login'),
+                    ),
+                    SizedBox(height: 15.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'You Don\'t have an account? ',
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            color: HexColor('#15BE77'),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: widget.showRegisterPage,
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              color: HexColor('#15BE77'),
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.h),
+                      child: Text(
+                        'Or Continue With',
+                        style: TextStyle(
+                            fontSize: 13.sp,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 0.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 120.w,
+                            height: 40.h,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.grey.withOpacity(0.4),
+                                  width: 0.2.w),
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 70,
+                                  offset: const Offset(2, 20),
+                                  color: HexColor('#15BE77').withOpacity(0.2),
+                                )
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image(
+                                  image: const AssetImage(
+                                    'assets/icons/facebook.png',
+                                  ),
+                                  width: 25.w,
+                                ),
+                                SizedBox(width: 8.w),
+                                Text(
+                                  'Facebook',
+                                  style: TextStyle(
+                                    fontSize: 13.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 10.w),
+                          Container(
+                            width: 120.w,
+                            height: 40.h,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.grey.withOpacity(0.4),
+                                  width: 0.2.w),
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 70,
+                                  offset: const Offset(2, 20),
+                                  color: HexColor('#15BE77').withOpacity(0.2),
+                                )
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image(
+                                  image: const AssetImage(
+                                    'assets/icons/google.png',
+                                  ),
+                                  width: 25.w,
+                                ),
+                                SizedBox(width: 8.w),
+                                Text(
+                                  'Google',
+                                  style: TextStyle(
+                                    fontSize: 13.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     )
