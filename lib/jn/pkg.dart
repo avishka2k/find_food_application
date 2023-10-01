01622900851219b0911db0c15c4437115f148fd2
Priyam <priyam05soni@gmail.com>
2023-10-01 15:15:26 +0530

    fix: Refactor Code to Reduce Code Duplication by Creating Custom Widgets

--- lib/jn/pkg.dart
+++ lib/jn/pkg.dart
@@ -1,69 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:find_food_application/widgets/custom_textformfield.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+  const MyWidget();
 
   @override
   State<MyWidget> createState() => _MyWidgetState();
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  TextEditingController _firstName = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        decoration: BoxDecoration(boxShadow: [
-          BoxShadow(
-            blurRadius: 100,
-            offset: const Offset(0, 20),
-            color: Colors.red.withOpacity(0.1),
-          )
-        ]),
-        child: TextFormField(
-          cursorColor: Colors.red,
-          keyboardType: TextInputType.emailAddress,
+    return Column(
+      children: [
+        CustomTextFormField(
+          controller: _firstName,
           validator: (value) {
             if (value!.isEmpty) {
               return 'Request';
             }
             return null;
           },
-          controller: _firstName,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 0.2.w,
-                  color: Colors.grey.withOpacity(0.5),
-                ),
-                borderRadius: BorderRadius.circular(15)),
-            hintText: 'First Name',
-            labelText: 'First Name',
-            labelStyle: const TextStyle(
-              color: Colors.grey,
-            ),
-            focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.red,
-                ),
-                borderRadius: BorderRadius.circular(15)),
-            focusedErrorBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.red,
-                ),
-                borderRadius: BorderRadius.circular(15)),
-            errorBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 0.2.w,
-                  color: Colors.red,
-                ),
-                borderRadius: BorderRadius.circular(15)),
-          ),
         ),
-      ),
-    ],);
+      ],
+    );
   }
 }
